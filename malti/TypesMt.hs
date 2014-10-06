01f18ff4ad0e9b9fec1328201da0f8c4886eaefc
John J. Camilleri <john@johnjcamilleri.com>
2014-10-06 19:15:10 +0200

    Add classifyRoot, char classes, other helpers

--- malti/TypesMt.hs
+++ malti/TypesMt.hs
@@ -5,9 +5,9 @@ module TypesMt where
 import General
 import Invariant
 
--- just for testing
+import qualified Data.String.Utils as DSU
 
-main = putStrLn "TypesMt"
+main = putStrLn "TypesMt" -- just for testing
 
 ------------------------------------------------------------------------------
 -- Trying to mimic GF syntax
@@ -16,6 +16,24 @@ main = putStrLn "TypesMt"
 
 type a ==> b = a -> b
 infixr ==>
+
+------------------------------------------------------------------------------
+-- Basic definitions
+
+type CharClass = [String]
+
+-- _Letter        :: [String] = [ "a" , "b" , "ċ" , "d" , "e" , "f" , "ġ" , "g" , "għ" , "h" , "ħ" , "i" , "ie" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "ż" , "z" ]
+_Consonant     :: [String] = [ "b" , "ċ" , "d" , "f" , "ġ" , "g" , "għ" , "h" , "ħ" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "ż" , "z" ]
+_CoronalCons   :: [String] = [ "ċ" , "d" , "n" , "r" , "s" , "t" , "x" , "ż" , "z" ] -- "konsonanti xemxin"
+_LiquidCons    :: [String] = [ "l" , "m" , "n" , "r" , "għ" ]
+-- _SonorantCons  :: [String] = [ "l" , "m" , "n" , "r" ] -- See {SA pg13}. Currently unused, but see DoublingConsN below
+_DoublingConsT :: [String] = [ "ċ" , "d" , "ġ" , "s" , "x" , "ż" , "z" ] -- require doubling when prefixed with 't', eg DDUM, ĠĠORR, SSIB, TTIR, ŻŻID {GM pg68,2b} {OM pg90}
+_DoublingConsN :: [String] = [ "l" , "m" , "r" ] -- require doubling when prefixed with 'n', eg LLAĦĦAQ, MMUR, RRID {OM pg90}
+_StrongCons    :: [String] = [ "b" , "ċ" , "d" , "f" , "ġ" , "g" , "għ" , "h" , "ħ" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "x" , "ż" , "z" ]
+_WeakCons      :: [String] = [ "j" , "w" ]
+_Vowel         :: [String] = [ "a" , "e" , "i" , "o" , "u" ]
+_VowelIE       :: [String] = [ "a" , "e" , "i" , "ie" , "o" , "u" ]
+_Empty         :: [String] = [ "" ]
 
 ------------------------------------------------------------------------------
 -- Parameter types for Maltese morphology
@@ -52,6 +70,8 @@ data Root = Root {c1, c2, c3, c4 :: String }
 
 data Verb = Verb {
     s :: VForm ==> VSuffixForm ==> Polarity ==> Str
+  , presPart :: GenNum ==> Str  -- Present/active particible, e.g. RIEQED
+  , pastPart :: GenNum ==> Str  -- Passive/past particible, e.g. MAĦBUB
   , i :: VerbInfo
   }
 
@@ -135,6 +155,106 @@ data VDerivedForm =
   | FormUnknown
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
 instance Param VDerivedForm where values = enum
+
+------------------------------------------------------------------------------
+-- Helpers
+
+classifyRoot :: Root -> VClass
+classifyRoot r
+  | (c4 r == "") = classifyRoot3 r
+  | otherwise    = classifyRoot4 r
+
+classifyRoot3 :: Root -> VClass
+classifyRoot3 r
+  | check3 (_WeakCons, _StrongCons, _StrongCons) = Weak Assimilative
+  | check3 (_StrongCons, _WeakCons, _StrongCons) = Weak Hollow
+  | check3 (_StrongCons, _StrongCons, _WeakCons) = Weak Lacking
+  | check3 (_StrongCons, _WeakCons, _WeakCons) = Weak Lacking
+  | check3 (_Consonant, _Consonant, ["għ"]) = Weak Defective
+  | check3 (_Consonant, _Consonant, _Consonant) =
+      if c2 r == c3 r
+      then Strong Geminated
+      else if c2 r `elem` _LiquidCons
+           then Strong LiquidMedial
+           else Strong Regular
+  | c1 r == "'" || c2 r == "'" || c3 r == "'" = Irregular
+  | otherwise = error $ "Cannot classify root: "++c1 r++"-"++c2 r++"-"++c3 r
+  where
+    check3 (t1,t2,t3) = c1 r `elem` t1 && c2 r `elem` t2 && c3 r `elem` t3
+
+classifyRoot4 :: Root -> VClass
+classifyRoot4 r
+  | check4 (_Consonant, _Consonant, _Consonant, _WeakCons) = Quad QWeak
+  | check4 (_Consonant, _Consonant, _Consonant, _Consonant) = Quad QStrong
+  | c1 r == "'" || c2 r == "'" || c3 r == "'" || c4 r == "'" = Irregular
+  | otherwise = error $ "Cannot classify root: "++c1 r++"-"++c2 r++"-"++c3 r++"-"++c4 r
+  where
+    check4 (t1,t2,t3,t4) = c1 r `elem` t1 && c2 r `elem` t2 && c3 r `elem` t3 && c4 r `elem` t4
+
+extractVowels :: String -> Vowels = \s ->
+  let
+    isVow x = [x] `elem` _Vowel
+    a = dropWhile (not.isVow) s
+    v1 = takeWhile (isVow) a
+    b = dropWhile (not.isVow) $ drop (length v1) a
+    v2 = takeWhile (isVow) b
+  in
+  Vowels v1 v2
+
+takePfx = take  -- takePfx 3 "hello" = "hel"
+dropPfx = drop  -- dropPfx 3 "hello" = "lo"
+takeSfx = dp    -- takeSfx 3 "hello" = "llo"
+dropSfx = tk    -- dropSfx 3 "hello" = "he"
+
+-- Prefix with a 'n'/'t' or double initial consonant, as necessary. See {OM pg 90}
+pfx_N :: String -> String
+pfx_N s
+  | s == "" = ""
+  | [head s] `elem` _DoublingConsN = (head s) : s
+  | otherwise = 'n' : s
+
+pfx_T :: String -> String
+pfx_T s
+  | s == "" = ""
+  | [head s] `elem` _DoublingConsT = (head s) : s
+  | otherwise = 't' : s
+
+pfx_J :: String -> String = \s -> pfx "j" s
+
+-- Generically prefix a string (avoiding empty strings)
+pfx :: String -> String -> String = \p s -> case (p,s) of {
+  (_ , "" ) -> [] ;
+  ("", str) -> str ;
+  (px, str) -> px ++ str
+  } ;
+
+-- -- Add suffix, avoiding blanks and triple letters {GO pg96-7}
+-- sfx :: String -> String -> String
+-- sfx "" _ = ""
+-- sfx kenn n
+--   | TODO
+
+-- sfx : String -> String -> String = \a,b ->
+--   case <a,takePfx 1 b> of {
+--     <"",_> -> [] ;
+--     <ke+"nn","n"> -> ke+"n"+b ;
+--     <ha+"kk","k"> -> ha+"k"+b ;
+--     <ho+"ll","l"> -> ho+"l"+b ;
+--     <si+"tt","t"> -> si+"t"+b ;
+--     <be+"xx","x"> -> be+"x"+b ;
+--     _ -> a + b
+--   } ;
+
+-- Replace any IE in the word with an I or E    --- potentially slow
+ie2i :: String -> String = ie2_ "i" ;
+ie2e :: String -> String = ie2_ "e" ;
+ie2_ :: String -> String -> String = \iore serviet ->
+  DSU.replace "ie" iore serviet
+  -- case serviet of {
+  --   x + "ie" => x + iore ;
+  --   x + "ie" + y => x + iore + y ;
+  --   x => x
+  -- } ;
 
 -- ===========================================================================
 
